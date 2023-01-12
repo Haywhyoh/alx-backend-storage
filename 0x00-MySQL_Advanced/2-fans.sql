2558ebcd798d77d616f7e5fa7e429fb92eb1b16c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-12 22:31:43 +0100

    a SQL script that ranks country origins of bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- a SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
-SELECT DISTINCT `origin`, SUM(fans) AS `n_fans`
+SELECT DISTINCT `origin`, SUM(fans) AS `n_fans` FROM `metal_bands`
 GROUP BY `origin`
 ORDER BY `n_fans`;
- 
