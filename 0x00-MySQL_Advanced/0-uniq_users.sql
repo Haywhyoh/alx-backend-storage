a265461d59dd01535e1efcefef7fead302da3030
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-12 14:00:24 +0100

    SQL script that creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
 -- SQL script that creates a table users
-USE holberton;
-CREATE TABLE IF NOT EXISTS holberton.users (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `email` VARCHAR(255) UNIQUE,
   `name` VARCHAR(255)
